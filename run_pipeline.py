34960077bd11bdb742cc8d479391212ba35bb3c8
grexai <grexai926@gmail.com>
2023-07-25 10:49:39 +0200

    fix args

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,12 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os.path
 import sys
 
-sys.path.append("ContrastiveUnpairedTranslation")
-sys.path.append("SuperPoint")
+sys.path.append(os.path.abspath("./ContrastiveUnpairedTranslation"))
+sys.path.append(os.path.abspath("./SuperPoint"))
 
 print(sys.path)
+from SuperPoint.SuperpointFunctions import *
 from SuperPoint.SuperpointFunctions import *
 from ContrastiveUnpairedTranslation import cut_funtions, util
 import time
@@ -30,35 +32,21 @@ if __name__ == '__main__':
     img2_file = args_glob.img2_path
     cut_weights_name = args_glob.cut_weights_name
     sp_weights_name = args_glob.sp_weights_name
-    cut_model = "D:/dev/python/contrastive-unpaired-translation/checkpoints/tissue_test_train_all"
-    sp_model_path = "d:/dev/python/SuperPoint/exper/saved_models/sp_v6/"
-    input_image = "D:/datasets/Image_registration/230123NH-77995rqivU/trainA/p1_wA1_t1_m1010_c0_z1_l1_o0.png"
-    # args.sp_weights_name
-    print(1)
-    m = cut_funtions.load_model(cut_model)
-    print(1)
-    sp_m = load_tensorflow_model(sp_model_path)
-    t = cut_funtions.convert_data_to_tensor(input_image)
+    # cut_model = "D:/dev/python/contrastive-unpaired-translation/checkpoints/tissue_test_train_all"
+    # sp_model_path = "d:/dev/python/SuperPoint/exper/saved_models/sp_v6/"
+    # input_image = "D:/datasets/Image_registration/230123NH-77995rqivU/trainA/p1_wA1_t1_m1010_c0_z1_l1_o0.png"
+    image_1 = args_glob.img1_path
+    image_2 = args_glob.img2_path
+
+    m = cut_funtions.load_model(cut_weights_name)
+    sp_m = load_tensorflow_model(sp_weights_name)
+    t = cut_funtions.convert_data_to_tensor(image_2)
     r = cut_funtions.inference(m, t)
     res_img = util.util.tensor2im(r)
-    # im = Image.fromarray(i m)
-    # im.save("testfake_B.png")
-    # define a transform to convert a tensor to PIL image
     img_size = (256, 256)
-    # im2 = test.convert_data_to_tensor("D:/datasets/Image_registration/230123NH-77995rqivU/trainB/p1_wA1_t1_m1010_c0_z1_l1_o0.png")
-    img1, img1_orig = preprocess_image(res_img, img_size)
-    img2, img2_orig = preprocess_image(res_img, img_size)
-    res = compute_superpoint(img1, img2)
-    print(res)
     start = time.time()
-    r = cut_funtions.inference(m, t)
-    res_img = util.util.tensor2im(r)
-    # im = Image.fromarray(i m)
-    # im.save("testfake_B.png")
-    # define a transform to convert a tensor to PIL image
-    img_size = (256, 256)
-    # im2 = test.convert_data_to_tensor("D:/datasets/Image_registration/230123NH-77995rqivU/trainB/p1_wA1_t1_m1010_c0_z1_l1_o0.png")
-    img1, img1_orig = preprocess_image(res_img, img_size)
+    i1 = cv2.imread(image_1, cv2.IMREAD_COLOR)
+    img1, img1_orig = preprocess_image(i1, img_size)
     img2, img2_orig = preprocess_image(res_img, img_size)
     res = compute_superpoint(img1, img2)
     end = time.time()
